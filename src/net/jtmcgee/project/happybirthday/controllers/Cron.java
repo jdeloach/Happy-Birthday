ea6fbe6add7602d46980a7d90f03fad81167f1cd
Jordan DeLoach <jordan@jtmcgee.net>
2011-06-15 17:57:45 -0500

    added email reporting to the Cron

--- src/net/jtmcgee/project/happybirthday/controllers/Cron.java
+++ src/net/jtmcgee/project/happybirthday/controllers/Cron.java
@@ -1,6 +1,17 @@
 package net.jtmcgee.project.happybirthday.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
@@ -22,29 +33,53 @@ public class Cron extends HttpServlet {
 
 	@SuppressWarnings("unchecked")
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		// look for todays birthdays
-			// get oauth token and post it
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		List<Birthday> bdays = (List<Birthday>) pm.newQuery(Birthday.class).execute();
-		for(Birthday bday: bdays) {
-			if(bday.isToday()) {
-				FacebookClient fbClient = new DefaultFacebookClient(bday.getToken());
-				FacebookType publishMessageResponse = fbClient.publish(bday.getUid() + "/feed", FacebookType.class, Parameter.with("message", bday.getMessage()));
-				resp.getWriter().println("Published message ID: " + publishMessageResponse.getId());
-			}
-		}
+		doPost(req, resp);
 	}
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		// look for todays birthdays
 			// get oauth token and post it
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		List<Birthday> bdays = (List<Birthday>) pm.newQuery(Birthday.class).execute();
+		List list = new ArrayList();
+		
 		for(Birthday bday: bdays) {
 			if(bday.isToday()) {
 				FacebookClient fbClient = new DefaultFacebookClient(bday.getToken());
 				FacebookType publishMessageResponse = fbClient.publish(bday.getUid() + "/feed", FacebookType.class, Parameter.with("message", bday.getMessage()));
 				resp.getWriter().println("Published message ID: " + publishMessageResponse.getId());
+				list.add("Result ID: " + publishMessageResponse.getId() + "\n" +
+						"Birthday wish to: " + bday.getUid() + ", From: " + bday.getWisher() + "\n" +
+						"Message: " + bday.getMessage());
+				// TODO rm birthday if it succeded (sp?)
 			}
 		}
+		
+		// MAIL
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        String msgBody = "";
+        try {
+	        for(int i = 0; i < list.size(); i++) {
+	        	msgBody = msgBody + "\n " + list.get(i);
+	        }
+        } catch(Exception e) {
+        	// this blows
+        }
+
+        try {
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("jordan@jtmcgee.net", "Jtmcgee.net Webmaster"));
+            msg.addRecipient(Message.RecipientType.TO,
+                             new InternetAddress("jordan@jtmcgee.net", "Jordan DeLoach"));
+            msg.setSubject("Results from the Birthday Wishes");
+            msg.setText(msgBody);
+            Transport.send(msg);
+
+        } catch (AddressException e) {
+            // ...
+        } catch (MessagingException e) {
+            // ...
+        }
 	}
 }
